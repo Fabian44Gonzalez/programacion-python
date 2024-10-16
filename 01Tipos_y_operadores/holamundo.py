35cabb5e16dede07897ccc834bc105d027fb7773
Fabián González Rodríguez <fabian205gonzlez@gmail.com>
2024-10-16 12:45:37 +0200

    piramides y ejercicios anteriores

---
+++ 01Tipos_y_operadores/holamundo.py
@@ -0,0 +1,6 @@
+num1 = int(input("Dame un número entero"))
+num2 = int(input("Dame un número entero"))
+if (num1 > num2):
+    print(num2)
+else:
+    print(num1)
---
+++ 02Estructuras_de_control/pruebas.py
@@ -0,0 +1,9 @@
+dia = int(input("Dime el día"))
+mes = int(input("Dime el mes"))
+año = int(input("Dime el año"))
+print(dia, mes, año)
+suma = dia + mes + año
+resultado = 0
+while resultado != 0:
+    suma_final = suma // 10
+    print(suma_final)    
